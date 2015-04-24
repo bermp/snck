5f869662322760fb1488a9b7bedaddfc0aa8aca8
Luis Collazo <luis.man.collazo@gmail.com>
2015-04-24 17:54:48 -0400

    image
    image on navbar

--- main.rb
+++ main.rb
@@ -2,20 +2,25 @@ require 'sinatra'
 require 'mandrill'
 require 'geolocater' #can be used for analytics or to require that user is in an area the site services
 
-get '/' do
+get '/home/' do
 	@title = "Snck"
     erb :home
 end
 
+get '/about/' 
+	do @title = "About"
+	erb :about 
+end
 
-get '/about' do @title = "About"
-	erb :about end
+get '/products/' do 
+	@title = "Products"
+	erb :products 
+end
 
-get '/products' do @title = "Products"
-	erb :products end
-
-get '/contact' do @title = "Contact"
-	erb :contact end
+get '/contact/' do 
+	@title = "Contact"
+	erb :contact 
+end
 
 post '/contact' do
 	puts params.inspect
@@ -41,5 +46,7 @@ post '/contact' do
 	redirect to('/thanks')
 end
 
-get '/thanks' do @title = "Thanks"
-	erb :thanks end
+get '/thanks/' do 
+	@title = "Thanks"
+	erb :thanks 
+end
